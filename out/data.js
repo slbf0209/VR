983985dd8877b756c09c6634ccd4d644f0b13317
slbf0209 <e02098070@gmail.com>
2026-05-22 10:44:31 +0800

    1

---
+++ out/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "摩天輪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 3.026078211827045,
+        "pitch": -0.38167912732511766,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1248641441230793,
+          "pitch": 0.2916318769340336,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.1097604133805845,
+          "pitch": 0.03071477750184215,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7894031518061233,
+          "pitch": -0.5790347324835814,
+          "title": "摩天輪",
+          "text": "摩天輪直徑40公尺並設有27個座艙，為園區主要地標，搭乘時可鳥瞰臺北盆地之美景。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "室外園區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.243224797780666,
+        "pitch": 0.25958299669152396,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0551332258998043,
+          "pitch": 0.2670512775148417,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.5801465908609007,
+          "pitch": 0.03363309534896608,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.84151056709047,
+          "pitch": 0.5328733301657511,
+          "title": "室外園區",
+          "text": "有海洋總動員、摩天輪、尋寶船、魔法星際飛車、宇宙迴旋、星空小飛碟等遊樂設施"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "尋寶船",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8665007447809945,
+          "pitch": 0.24919883326290915,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 3.1097628508876216,
+          "pitch": -0.3967419510480177,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9945608917773807,
+          "pitch": 0.000348051348721512,
+          "title": "尋寶船",
+          "text": "仿木造古戰船的造型，以左右擺盪凌空飛渡，體驗離心力的刺激感。"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "海洋總動員",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.334856720594388,
+          "pitch": 0.05113122092453892,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.868182479089512,
+          "pitch": 0.07079887785344852,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.742717672223872,
+          "pitch": 0.20509129320394592,
+          "title": "海洋總動員",
+          "text": "以海洋生物為載具造型的音樂馬車，上方有多樣臺灣海域之海洋生物彩繪圖案，以增加小朋友的教育認識。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "魔法星際飛車",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7756224577013118,
+          "pitch": -0.0023353530141019263,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.404676932687561,
+          "pitch": 0.15030309845827006,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2626681950025844,
+          "pitch": 0.11808936037423479,
+          "title": "魔法星際飛車",
+          "text": "以科幻造型的雲霄飛車，迴旋於軌道上體驗刺激的速度感。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
