a12101759c19b47b1a70541e34aed8e4ccbc3e70
slbf0209 <e02098070@gmail.com>
2026-05-22 11:13:29 +0800

    1

---
+++ in/data.js
@@ -0,0 +1,215 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "室內園區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.9893198094948445,
+        "pitch": -0.08396061325923654,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2785833293651354,
+          "pitch": 0.10155505455208136,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.591505776545567,
+          "pitch": -0.12667331902796164,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7352327136118042,
+          "pitch": 0.23760142040861787,
+          "title": "室內園區",
+          "text": "室內園區設有豐富的室內球池、遊戲場與DIY體驗區，無懼風雨皆能盡情暢玩<img src=\"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSiPFrCSDQBsbDmUemPUfYAJHR68ac1spE7cpVNLVX6lYgCS0GuoAq7Q5kUViXh\" alt=\"台北市兒童新樂園室內遊戲區 (由 AI 生成)\"><div><div _ngcontent-ng-c3357112061=\"\" inline-copy-host=\"\" class=\"markdown markdown-main-panel stronger enable-updated-hr-color\" id=\"model-response-message-contentr_0ec69974c5b0eda8\" aria-live=\"polite\" aria-busy=\"false\" dir=\"ltr\" style=\"--animation-duration: 400ms; --fade-animation-function: ease-out; animation: auto ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px rgb(31, 31, 31); inset: auto; clear: none; clip: auto; color: rgb(31, 31, 31); columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; display: flex; fill: rgb(0, 0, 0); filter: none; flex: 0 1 auto; float: none; gap: 16px; hyphens: manual; interactivity: auto; isolation: auto; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(31, 31, 31) none 2.4px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1; margin-top: 0px !important; font-family: &quot;Google Sans Text&quot;, sans-serif !important; line-height: 1.15 !important;\"><div style=\"animation: auto ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px rgb(31, 31, 31); inset: auto; clear: none; clip: auto; columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; fill: rgb(0, 0, 0); filter: none; flex: 0 1 auto; float: none; gap: normal; hyphens: manual; interactivity: auto; isolation: auto; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(31, 31, 31) none 2.4px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1; margin-top: 0px !important; line-height: 1.15 !important;\"><div class=\"attachment-container search-images\" style=\"animation: auto ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px rgb(31, 31, 31); inset: auto; clear: none; clip: auto; columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; display: flex; fill: rgb(0, 0, 0); filter: none; flex: 0 1 auto; float: none; gap: 8px; hyphens: manual; interactivity: auto; isolation: auto; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(31, 31, 31) none 2.4px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1; margin-top: 0px !important; line-height: 1.15 !important;\"><response-element class=\"\" ng-version=\"0.0.0-PLACEHOLDER\" style=\"animation: auto ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px rgb(31, 31, 31); inset: auto; clear: none; clip: auto; columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; display: block; fill: rgb(0, 0, 0); filter: none; flex: 0 1 auto; float: none; gap: normal; hyphens: manual; interactivity: auto; isolation: auto; margin-right: 247px; margin-bottom: 0px; margin-left: 247px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(31, 31, 31) none 2.4px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1; margin-top: 0px !important; line-height: 1.15 !important;\"><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><single-image _nghost-ng-c3868995075=\"\" data-is-interactive=\"false\" class=\"spark-licensed-center-host luminous-layout ng-star-inserted\" data-image-attachment-index=\"0\" style=\"animation: auto ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px rgb(31, 31, 31); inset: auto; clear: none; clip: auto; columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; display: inline; fill: rgb(0, 0, 0); filter: none; flex: 0 1 auto; float: none; gap: normal; hyphens: manual; interactivity: auto; isolation: auto; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(31, 31, 31) none 2.4px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1; margin-top: 0px !important; line-height: 1.15 !important;\"><!----><!----><!----><!----><!----><!----><!----></single-image><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----></response-element></div></div></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "球池",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.1888538262713126,
+        "pitch": 0.5270468412684508,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31887753925310136,
+          "pitch": 0.022346567826865282,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.8166223068270799,
+          "pitch": -0.16063043536571797,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8579827959399005,
+          "pitch": 0.48962149913477404,
+          "title": "球池",
+          "text": "滿滿彩色球池與溜滑梯，是孩子們放電的室內天堂<img src=\"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTumeZK__oWwQJx38bNIJ5jCAHiyICfkskHGgguJ0fmuKaSkPfarMsbvyyZhdHP\" alt=\"台北市兒童新樂園室內球池遊戲場 (由 AI 生成)\">"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "美食街",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.5378413603448671,
+        "pitch": 0.07887082000783074,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3211228474547543,
+          "pitch": 0.1384454219213218,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.5976299128764087,
+          "pitch": 0.2619154006208877,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5437061761234965,
+          "pitch": 0.24261676868439253,
+          "title": "美食街",
+          "text": "園區二、三樓設有美食街，提供台式小吃、速食與甜點，方便全家隨時補給體力<div><img src=\"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRm4fmEXtz4b99eeY1CjfGCtaeraVV-BjypwAFFV0yckIvRfR8d_9tgs1zVG-6t\" alt=\"台北市兒童新樂園美食街 (由 AI 生成)\"></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "旋轉咖啡杯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0.17631387341574367,
+        "pitch": 0.05851800318068001,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.310106096254791,
+          "pitch": 0.08706677916156202,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 3.138521806609395,
+          "pitch": 0.1245156373515286,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2976248123448748,
+          "pitch": 0.16059018712202544,
+          "title": "旋轉咖啡杯",
+          "text": "園區三樓棚頂設有轉轉咖啡杯，隨盤旋轉眩目耀眼，更有大屋頂遮陽避雨<div><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYuJljQ2W2EG6_yEuxXgbDFBbBFl8y1-eKaZZnNFcOQissk_k9ICY3spqCl3Mm\" alt=\"台北市兒童新樂園 轉轉咖啡杯 設施 (由 AI 生成)\"></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
